4edf736203ae60370bb4f5673c5ded241c95301b
langstonn2026 <langston.bennett@hgs.hiddengeniusproject.org>
2024-09-17 18:54:42 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,9 @@
-function darkMode() {
-// Your code here.
+function darkMode(){
+    if(document.body.style.backgroundColor=== "white"){
+        document.body.style.backgroundColor="blue";
+        document.body.style.backgroundColor="white";
+    }else {
+        document.body.style.backgroundColor="white";
+        documnet.body.style.color="black";
+    }
 }
