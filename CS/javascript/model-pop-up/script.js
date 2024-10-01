f38102e45b27602a3866c1d8d018a34639e45363
langstonn2026 <langston.bennett@hgs.hiddengeniusproject.org>
2024-09-30 17:09:17 -0700

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,29 +1,29 @@
 // Get modal element
-const modal=document.getElementById("myModak");
+const modal = document.getElementById("myModal");
 
 // Get buttons
-const openModalBtn= document.getElementById("openModalBtn");
-const closeModalBtn= document.getElementById("closeModalBtn");
-const alertBtn= document.getElementById("alertBtn");
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const alertBtn = document.getElementById("alertBtn");
 
 // Open modal on button click 
-openModalBtn.onclick=function(){
-    modal.style.display="block";
+openModalBtn.onclick = function(){
+    modal.style.display = "block";
 }
 
 // Open modal on button click 
-openModalBtn.onclick=function(){
-    modal.style.display="none";
+closeModalBtn.onclick=function(){
+    modal.style.display = "none";
 }
 
 // Open modal on button click 
 window.onclick=function(event){
-    if (event.target=== modal){
-        modal.style.display="none";
+    if (event.target === modal){
+        modal.style.display = "none";
     }
    
 }
 // show alert on button click
-alertBtn.onclick=function(){
+alertBtn.onclick = function(){
     alert(" this is a simple pop up alert HOW ABOUT THEM COWBOYS ")
 }
