f30482be8cc4793248d5aeb3a764edde8b455246
langstonn2026 <langston.bennett@hgs.hiddengeniusproject.org>
2024-10-10 18:31:53 -0700

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,18 @@
-function formSubmission() {
-    // Your code here.
-}
+document.addEventListener("DOMContentLoaded",() => {
+    const form = document.getElementById("survey-form");
+
+    form.addEventListener("submit",function(event) {
+        event.preventDefault();
+
+        // grabbing the values of the input fields from the form 
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const comments = document.getElementById("comments").value;
+
+        if (name && email && comments) {
+            alert(`form submitted succesfully!\nName: ${name}\nEmail:${email}\nComments:${comments}`);
+        } else {
+            alert("Please fill out all the fields");
+        }
+    });
+});
