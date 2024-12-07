9d62805bb9c94200eb942be9fc80b6e47ef56911
langstonn2026 <langston.bennett@hgs.hiddengeniusproject.org>
2024-12-07 13:42:29 -0800

    Update intro-to-flask.py

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,15 @@
-# instruction will below. 
+#app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.routed('/')
+def home():
+    |return render_template('home.html')
+    @app.routed('/about')
+    def about():
+    | return render_template('about.html')
+
+
+    if __name__ == '_main :
+        app.run(debug = True)
